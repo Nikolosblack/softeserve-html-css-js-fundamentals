81509c2c6893108b497162d6d36ef96c3372d8b2
Nikolos Black <Nikolosblack@gmail.com>
2022-02-06 22:17:58 +0200

    add footer html and css with media queries

--- workout-brand-slider/js/burger.js
+++ workout-brand-slider/js/burger.js
@@ -1,5 +1,26 @@
-var wrapperMenu = document.querySelector('.wrapper-menu');
 
-wrapperMenu.addEventListener('click', function(){
-  wrapperMenu.classList.toggle('open');  
-})
+(function () {
+    const iconBurger = document.querySelector(".wrapper-menu");
+    const navigationsBody = document.querySelector(".navigation-wrapper");
+    iconBurger.addEventListener("click", function (e) {
+      document.body.classList.toggle("lock");
+      iconBurger.classList.toggle("active");
+      iconBurger.classList.toggle("open");
+      navigationsBody.classList.toggle("active");
+
+    });
+
+
+  
+    const navLinks = document.querySelectorAll(".navigation__link");
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener("click", (_) => {
+            if (iconBurger.classList.contains("active")) {
+                document.body.classList.remove("lock");
+                iconBurger.classList.remove("active");
+                navigationsBody.classList.remove("active");
+            }
+          }
+      );
+    });
+  })();
