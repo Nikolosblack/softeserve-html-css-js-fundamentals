48435b3978b73bc93b1f8c0d01c43c98749a62a9
Nikolos Black <Nikolosblack@gmail.com>
2022-02-14 22:26:02 +0200

    update modal section

--- workout-brand-slider/js/submit-popup.js
+++ workout-brand-slider/js/submit-popup.js
@@ -4,8 +4,14 @@ const modalClose = document.querySelector(".modal-close");
 
 modalBtn.addEventListener("click", function(){
     modalBg.classList.add("modal-bg-active");
+    setTimeout(function() {
+        modalBg
+    }, 4000);
 });
+
+
 
 modalClose.addEventListener("click", function(){
     modalBg.classList.remove("modal-bg-active");
 })
+setTimeout(click, 4000)
